f8cf456ac1da8bb5b5513f23d1c332fd3c2e4ac6
Constantine Philipchenko <konstantin-649@mail.ru>
2020-12-25 21:38:33 +0300

    Добавлен тестовый проект #13

---
+++ source/Tests/Library.NH.Tests/AuthorMapTests.cs
@@ -0,0 +1,18 @@
+namespace Library.NH.Tests
+{
+    using NUnit.Framework;
+
+    public class AuthorMapTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
